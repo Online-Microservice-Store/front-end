457305a884d895510e824a53a693f41723dcef49
JhairAgila <jhairagil@gmail.com>
2025-07-18 12:24:10 -0500

    fix/build

--- app/src/app/pages/api/auth/[...nextauth]/route.ts
+++ app/src/app/pages/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,26 @@
-import NextAuth from "next-auth"
+// import NextAuth from "next-auth"
+// const handler = NextAuth({
+//   ...
+// })
+// export { handler as GET, handler as POST }
+//After fix build:
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
 
 const handler = NextAuth({
-  ...
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Username', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        // Lógica de autenticación
+        return { id: '1', name: 'Jhair' }
+      },
+    }),
+  ],
 })
 
 export { handler as GET, handler as POST }
